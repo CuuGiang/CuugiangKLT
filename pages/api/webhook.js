cb8ca5a8c8755869d4dd6ba4d36c5f5f0fa2ad97
CuuGiang <dienlanhcuugiang@gmail.com>
2025-02-16 16:07:32 +0700

    Update webhook.js

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,8 +1,8 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
-        console.log(req.body);  // Kiểm tra dữ liệu nhận được
+        console.log(req.body);  // Kiểm tra dữ liệu nhận được từ Zalo OA
         res.status(200).send('OK');  // Trả về HTTP 200 OK
     } else {
-        res.status(405).send('Method Not Allowed');  // Trả về HTTP 405 nếu không phải POST
+        res.status(405).send('Method Not Allowed');  // Nếu không phải POST
     }
 }
